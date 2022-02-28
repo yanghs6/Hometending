9c04c845fc5b7b7826c9c5221d80da2400f864ec
yanghs6 <yanghs632@gmail.com>
2022-02-28 09:01:32 +0900

    [기능] 칵테일 페이지 추가

---
+++ cocktail/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Recipe(models.Model):
+    name=models.CharField(max_length=50)
+    glass=models.CharField(max_length=50)
+    garnish=models.CharField(max_length=50)
+    basesprite=models.CharField(max_length=50)
+    ingredient=models.TextField()
+    technique=models.CharField(max_length=50)
+    def __str__(self):
+        return f"id={self.id},glass={self.glass}, garnish={self. garnish},basesprite={self.basesprite},ingredient={self.ingredient},technique={self.technique}"
+
+
+# Create your models here.
