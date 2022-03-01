3f5ae805601c02bc367302af8802370e5db0072f
yanghs6 <yanghs632@gmail.com>
2022-03-01 22:36:01 +0900

    [기능] 게시판 추가

--- hometending/urls.py
+++ hometending/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('firstpage/', include('firstpage.urls')),
     path('cocktail/', include('cocktail.urls')),
+    path('boardapp/', include('boardapp.urls')),
     path('account/', include('accountapp.urls')),
 ]
