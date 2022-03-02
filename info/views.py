a67b2a3bdfc3e7767f614398e5c4a0a1e3fb942d
yanghs6 <yanghs632@gmail.com>
2022-03-02 14:46:26 +0900

    [기능] index, about 페이지 추가, 부트스트랩 아이콘 CDN 추가

--- info/views.py
+++ info/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about(reqeust):
+    context = {'member':
+                [
+                    {'name':'Yeonseo Park', 'role':'칵테일 정보 기능', 'githubid':'YSPARK525', 'email':None},
+                    {'name':'HaeSeong Yang', 'role':'회원가입 기능', 'githubid':'yanghs6', 'email':'yanghs632@gmail.com'},
+                    {'name':'Yeongjun Lee',  'role':'커뮤니티 게시판 기능', 'githubid':'gotitgitgit', 'email':None}
+                ]
+              }
+    
+    return render(reqeust, 'about.html', context)
