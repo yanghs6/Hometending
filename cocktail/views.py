184145cc0996226986f21566cf015044491f1ee0
Yeonseo <yyeonseo.park@gmail.com>
2022-03-03 09:27:00 +0900

    칵테일 리스트 생성

--- cocktail/views.py
+++ cocktail/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from .models import Recipe
+
 
 
 def list(request):
-    context = dict()
-
+    cocktail_list=Recipe.objects.order_by('pk')
+    context = {'cocktail_list':cocktail_list}
     return render(request, "list.html", context)
 
-def recipes(request):
-    context = dict()
-
+def recipes(request,cocktail_id):
+    recipe=Recipe.objects.get(id=cocktail_id)
+    context = {'recipe': recipe}
     return render(request, "recipes.html", context)
 
+
+
+def basesprite(request):
+    base_list=Recipe.objects.order_by('basesprite').values('basesprite').distinct()
+    context = {'base_list': base_list}
+    return render(request, "basesprite.html", context)
+
+def listbybase(request,base_name):
+    lbyb=Recipe.objects.filter(basesprite=base_name)
+    context = {'lbyb': lbyb}
+    return render(request, "listbybase.html", context)
+
 # Create your views here.
+
