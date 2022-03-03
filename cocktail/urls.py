c457f0662ae8217afc686dfca071d5ad4b153634
Yeonseo <yyeonseo.park@gmail.com>
2022-03-03 16:03:11 +0900

    검색 기능

--- cocktail/urls.py
+++ cocktail/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.list),
+    path("", views.list, name='list'),
     path("<int:cocktail_id>/", views.recipes),
     path("basesprite/", views.basesprite),
     path("basesprite/<base_name>/", views.listbybase),
--- cocktail/views.py
+++ cocktail/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from .models import Recipe
-
+from django.db.models import Q
 
 
 def list(request):
-    cocktail_list=Recipe.objects.order_by('pk')
-    context = {'cocktail_list':cocktail_list}
+    # 조회
+    cocktail_list = Recipe.objects.order_by('pk')
+    kw = request.GET.get('kw', '')  # 검색어
+    # 검색
+    if kw:
+        cocktail_list = cocktail_list.filter(
+            Q(name__icontains=kw) |  # 이름검색
+            Q(basesprite__icontains=kw) |  # 베이스검색
+            Q(ingredient__icontains=kw) |  # 재료검색
+            Q(garnish__icontains=kw)  #가니시검색
+        ).distinct()
+
+    context = {'cocktail_list':cocktail_list, 'kw': kw}
     return render(request, "list.html", context)
+
+
 
 def recipes(request,cocktail_id):
     recipe=Recipe.objects.get(id=cocktail_id)
     context = {'recipe': recipe}
     return render(request, "recipes.html", context)
-
-
 
 def basesprite(request):
     base_list=Recipe.objects.order_by('basesprite').values('basesprite').distinct()
