b9376fc43fddf47cb29ac01e81daeba3a1a8f2ac
yanghs6 <yanghs632@gmail.com>
2022-03-04 16:03:58 +0900

    [수정] 네비게이션바 로그인 상태 수정

--- hometending/settings.py
+++ hometending/settings.py
@@ -61,7 +61,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'boardapp.apps.BoardappConfig',
-    'common.apps.CommonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hometending/urls.py
+++ hometending/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('cocktail/', include('cocktail.urls')),
     path('boardapp/', include('boardapp.urls')),
     path('account/', include('accountapp.urls')),
-    path('info/', include('common.urls')),
+    # path('info/', include('common.urls')),
     path('admin/', admin.site.urls),
 ]
