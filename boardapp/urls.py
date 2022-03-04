b024b40a707da34b3040c1632dba1aa87db7c6ba
Yeongjun Lee <99387875+gotitgitgit@users.noreply.github.com>
2022-03-04 11:57:10 +0900

    Add files via upload

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -31,3 +31,4 @@ urlpatterns = [
     path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
     path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 ]
+
