41dc2956356d4f93834a03de0b167a3b0d263bd8
Yeonseo <yyeonseo.park@gmail.com>
2022-03-04 16:04:05 +0900

    칵테일 사진 리스트 생성

--- cocktail/views.py
+++ cocktail/views.py
@@ -5,18 +5,19 @@ from django.db.models import Q
 
 def list(request):
     # 조회
-    cocktail_list = Recipe.objects.order_by('pk')
+    cocktail_alllist = Recipe.objects.order_by('pk')
     kw = request.GET.get('kw', '')  # 검색어
     # 검색
     if kw:
-        cocktail_list = cocktail_list.filter(
+        cocktail_alllist = cocktail_alllist.filter(
             Q(name__icontains=kw) |  # 이름검색
             Q(basesprite__icontains=kw) |  # 베이스검색
             Q(ingredient__icontains=kw) |  # 재료검색
             Q(garnish__icontains=kw)  #가니시검색
         ).distinct()
-
-    context = {'cocktail_list':cocktail_list, 'kw': kw}
+    n=4
+    cocktail_list = [cocktail_alllist[i * n:(i + 1) * n] for i in range((len(cocktail_alllist) + n - 1) // n)]
+    context = {'cocktail_alllist':cocktail_alllist,'cocktail_list':cocktail_list, 'kw': kw}
     return render(request, "list.html", context)
 
 
