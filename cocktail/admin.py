1763840188071cb5fc9c6bc7770bc5463e6b308c
yanghs6 <yanghs632@gmail.com>
2022-03-15 11:05:07 +0900

    [수정] Recipe admin 변경, Recipe 모델 ingredient 필드를 recipe으로 변경

--- cocktail/admin.py
+++ cocktail/admin.py
@@ -4,8 +4,8 @@ from .models import Cocktail
 
 # Register your models here.
 class CocktailAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'glass', 'garnish', 'basesprite', 'ingredient', 'technique']
-    list_display = ['name', 'glass', 'garnish', 'basesprite', 'ingredient', 'technique']
+    search_fields = ['name', 'glass', 'garnish', 'basesprite', 'recipe', 'technique']
+    list_display = ['name', 'glass', 'garnish', 'basesprite', 'recipe', 'technique']
 
     
 admin.site.register(Cocktail, CocktailAdmin)
--- cocktail/models.py
+++ cocktail/models.py
@@ -5,7 +5,7 @@ class Cocktail(models.Model):
     glass=models.CharField(max_length=50)
     garnish=models.CharField(max_length=50)
     basesprite=models.CharField(max_length=50)
-    ingredient=models.TextField()
+    recipe=models.TextField()
     technique=models.CharField(max_length=50)
     def __str__(self):
         return f"id={self.id},glass={self.glass}, garnish={self. garnish},basesprite={self.basesprite},ingredient={self.ingredient},technique={self.technique}"
