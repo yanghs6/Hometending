70450ad2e2175ef31077f00cb509eeaf21631de3
yanghs6 <yanghs632@gmail.com>
2022-03-15 10:20:05 +0900

    [수정] accountapp docstring 추가, 비밀번호 길이 제한

--- accountapp/admin.py
+++ accountapp/admin.py
@@ -3,6 +3,13 @@ from .models import *
 
 
 class HometendAdmin(admin.ModelAdmin):
+    """모델 HometendUser의 admin
+    
+    Attributes:
+        search_fields: username, first_name, email, date_joined, last_login
+        list_display: username, first_name, email, date_joined, last_login
+        list_filter: date_joined, last_login
+    """
     search_fields = ['username', 'first_name', 'email', 'date_joined', 'last_login']
     list_display = ['username', 'first_name', 'email', 'date_joined', 'last_login']
     list_filter = ['date_joined', 'last_login']
